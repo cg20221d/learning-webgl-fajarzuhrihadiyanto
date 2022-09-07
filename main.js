9c3ec2473e4dc1944911b6f02f1c421ed8ea4afe
Fajar Zuhri H <fajarzuhri76@gmail.com>
2022-09-07 16:24:42 +0700

    feat: simple background paint

--- main.js
+++ main.js
@@ -1,3 +1,40 @@
 const main = () => {
-    
+    const myCanvas = document.getElementById('my-canvas')
+    const gl = myCanvas.getContext('webgl')
+
+    //#region  //*=========== Vertex Shader ===========
+    let vertexShaderCode = 
+    `
+        void main() {
+
+        }
+    `
+    const vertexShaderObject = gl.createShader(gl.VERTEX_SHADER)
+    gl.shaderSource(vertexShaderObject, vertexShaderCode)
+    gl.compileShader(vertexShaderObject)
+    //#endregion  //*======== Vertex Shader ===========
+
+    //#region  //*=========== Fragment Shader ===========
+    let fragmentShaderCode =
+    `
+        void main() {
+
+        }
+    `
+    const fragmentShaderObjext = gl.createShader(gl.FRAGMENT_SHADER)
+    gl.shaderSource(fragmentShaderObjext, fragmentShaderCode)
+    gl.compileShader(fragmentShaderObjext)
+    //#endregion  //*======== Fragment Shader ===========
+
+
+    //#region  //*=========== Create Executable Container ===========
+    const shaderProgram = gl.createProgram()
+    gl.attachShader(shaderProgram, vertexShaderObject)
+    gl.attachShader(shaderProgram, fragmentShaderObjext)
+    gl.linkProgram(shaderProgram)
+    gl.useProgram(shaderProgram)
+    gl.clearColor(0.38, 0.51, 0.96, 1)
+    gl.clear(gl.COLOR_BUFFER_BIT)
+    //#endregion  //*======== Create Executable Container ===========
+
 }
