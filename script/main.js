5c9ed84b59bc8385930b543e54bffc786309b7a1
Fajar Zuhri H <fajarzuhri76@gmail.com>
2022-09-14 14:52:42 +0700

    feat: add simple points

--- script/main.js
+++ script/main.js
@@ -1,3 +1,5 @@
+// @type (HTMLCanvasElement)
+
 const main = () => {
     const myCanvas = document.getElementById('my-canvas')
     const gl = myCanvas.getContext('webgl')
@@ -6,7 +8,10 @@ const main = () => {
     let vertexShaderCode = 
     `
         void main() {
-
+            float x = 0.0;
+            float y = 0.0;
+            gl_PointSize = 50.0;
+            gl_Position = vec4(x, y, 0.0, 1.0);
         }
     `
     const vertexShaderObject = gl.createShader(gl.VERTEX_SHADER)
@@ -17,8 +22,12 @@ const main = () => {
     //#region  //*=========== Fragment Shader ===========
     let fragmentShaderCode =
     `
+        precision mediump float;
         void main() {
-
+            float r = 0.96;
+            float g = 0.68;
+            float b = 0.26;
+            gl_FragColor = vec4(r, g, b, 1.0);
         }
     `
     const fragmentShaderObjext = gl.createShader(gl.FRAGMENT_SHADER)
@@ -37,4 +46,8 @@ const main = () => {
     gl.clear(gl.COLOR_BUFFER_BIT)
     //#endregion  //*======== Create Executable Container ===========
 
+
+    //#region  //*=========== Draw Points ===========
+    gl.drawArrays(gl.POINTS, 0, 1)
+    //#endregion  //*======== Draw Points ===========
 }
